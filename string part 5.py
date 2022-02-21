1099765f02d993f9f8d87af9c797b20e3b9b2449
SUSHMAGANDAM <99244274+SUSHMAGANDAM@users.noreply.github.com>
2022-02-21 21:37:21 +0530

    Add files via upload

---
+++ string part 5.py
@@ -0,0 +1,20 @@
+# a=[float(x) for x in input().split(" ")]
+# print(a,type(a))
+# print(sum(a))
+# print(a[0]+a[1]+a[2]+a[3])
+# print("the sum is"+" "+str(sum(a)))
+# print("the sum is",sum(a))
+# m="the python  made world became more easy"
+# print(m.split())
+# h="welcome:to:python:world"
+# print(h.split(':'))
+n="python#java#c#c##dart"
+print(n.split("#"))
+print(n.split("#",0))
+print(n.split("#",1))
+print(n.split("#",2))
+print(n.split("#",3))
+print(n.split("#",4))
+print(n.split("#",5))
+print(n.split("#",6))
+print(n.split("#",7))
